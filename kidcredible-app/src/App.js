ddbbfac43dce23b7b9f5f383e25076a8cfd722fa
Justin Harris <justin.r.harris@gmail.com>
2022-12-22 17:54:36 -0500

    added components

--- kidcredible-app/src/App.js
+++ kidcredible-app/src/App.js
@@ -1,12 +1,25 @@
 import "./App.css"
 import { Route, Routes } from "react-router-dom"
+import Nav from "./components/Nav"
+import Home from "./components/Home"
+import Header from "./components/Header"
+import Products from "./component/Products"
+import ProductDetails from "./component/ProductDetails"
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <h1> hello world </h1>
+      <header className="Nav">
+        <Header></Header>
+        <Nav></Nav>
       </header>
+      <div>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/products" element={<Products />} />
+          <Route path="/products/:id" element={<ProductDetails />} />
+        </Routes>
+      </div>
     </div>
   )
 }
