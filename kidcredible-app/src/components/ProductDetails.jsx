2df45228bdada1019a4be1de68917cb4c5c588af
Justin Harris <justin.r.harris@gmail.com>
2022-12-22 20:49:05 -0500

    data is rendering

--- kidcredible-app/src/components/ProductDetails.jsx
+++ kidcredible-app/src/components/ProductDetails.jsx
@@ -1,3 +1,46 @@
-export default function ProductDetails() {
-  return
+import { useState, useEffect } from "react"
+import axios from "axios"
+
+export default function productDetails() {
+  const [products, setProducts] = useState()
+
+  useEffect(() => {
+    const getProducts = async () => {
+      const response = await axios.get("http://localhost:8000/products")
+      setProducts(response.data)
+      console.log(response.data)
+    }
+
+    getProducts()
+  }, [])
+
+  if (!products) {
+    return <h2> Loading Please Wait</h2>
+  } else {
+    return (
+      <div>
+        {products.map((product, i) => (
+          <div key={product.productname} className="card">
+            <div className="flex justify-center productImageCard">
+              <div className="flex justify-center productImage">
+                <img src={product.image} />
+              </div>
+            </div>
+            <div className="productDetails">
+              <div className="flex justify-center productName">
+                <h3> {product.name} </h3>
+              </div>
+
+              <div className="flex justify-center productType">
+                <h3> {product.type} </h3>
+              </div>
+              <div className="flex justify-center productDescription">
+                <h3>{product.description}</h3>
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
+    )
+  }
 }
--- kidcredible-app/src/components/Products.jsx
+++ kidcredible-app/src/components/Products.jsx
@@ -2,7 +2,7 @@ import { useState, useEffect } from "react"
 import axios from "axios"
 
 export default function Products() {
-  const [products, setProducts] = useState
+  const [products, setProducts] = useState()
 
   useEffect(() => {
     const getProducts = async () => {
@@ -14,9 +14,33 @@ export default function Products() {
     getProducts()
   }, [])
 
-  return (
-    <div>
-      <h1> testing testing </h1>
-    </div>
-  )
+  if (!products) {
+    return <h2> Loading Please Wait</h2>
+  } else {
+    return (
+      <div>
+        {products.map((product, i) => (
+          <div key={product.productname} className="card">
+            <div className="flex justify-center productImageCard">
+              <div className="flex justify-center productImage">
+                <img src={product.image} />
+              </div>
+            </div>
+            <div className="productDetails">
+              <div className="flex justify-center productName">
+                <h3> {product.name} </h3>
+              </div>
+
+              <div className="flex justify-center productType">
+                <h3> {product.type} </h3>
+              </div>
+              <div className="flex justify-center productDescription">
+                <h3>{product.description}</h3>
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
+    )
+  }
 }
