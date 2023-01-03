a437e40de0c4ec09bfe5876b704c88abf0960b92
Justin Harris <justin.r.harris@gmail.com>
2023-01-03 11:34:34 -0500

    styling

--- kidcredible-app/src/components/Nav.jsx
+++ kidcredible-app/src/components/Nav.jsx
@@ -1,16 +1,36 @@
 import { Link } from "react-router-dom"
+import HeaderPicture from "/Users/joliewoodson/Desktop/GA/unit4/Project 4/Kidcredible/Kidcredible_frontend/kidcredible-app/src/assests/kidcredible-header.png"
 
 export default function Nav(props) {
   return (
     <div className="nav">
-      <Link to="/">
-        {" "}
+      <link rel="preconnect" href="https://fonts.googleapis.com" />
+      <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+
+      <link
+        href="https://fonts.googleapis.com/css2?family=Butterfly+Kids&display=swap"
+        rel="stylesheet"
+      />
+      <style>
+        @import
+        url('https://fonts.googleapis.com/css2?family=Butterfly+Kids&display=swap');
+      </style>
+      <div className="headerImages">
         <h2 className="homeLink">Kidcredible</h2>{" "}
-      </Link>
-      <Link to="/">
+        <Link to="/">
+          {" "}
+          <img
+            classname="headerImage"
+            src={HeaderPicture}
+            width="50%"
+            height="auto"
+          />
+        </Link>
+      </div>
+      {/* <Link to="/">
         {" "}
         <h2 className="homeLink">Home</h2>{" "}
-      </Link>
+      </Link> */}
     </div>
   )
 }
